b373a2c843b950b0306f3a014a0f9688f9a1db7d
Federico Bermudez <fedeberm@192.168.1.33>
2026-05-09 02:22:14 -0300

    Fix next.config for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
   async headers() {
     return [
       {
